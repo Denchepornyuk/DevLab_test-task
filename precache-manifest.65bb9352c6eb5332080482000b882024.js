abed7af023bdfb9e5d5e98765d40d1dd48b8e080
Denys <denys.cheporniuk@gmail.com>
2020-02-19 18:41:04 +0200

    Updates

---
+++ precache-manifest.65bb9352c6eb5332080482000b882024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b31bd31823de8f343c081d27350520",
+    "url": "/Nimble_test-task/index.html"
+  },
+  {
+    "revision": "d990e67235fe187a6784",
+    "url": "/Nimble_test-task/static/css/main.a8d0aeb7.chunk.css"
+  },
+  {
+    "revision": "ff12a30dac7a490a0807",
+    "url": "/Nimble_test-task/static/js/2.4df356bd.chunk.js"
+  },
+  {
+    "revision": "d990e67235fe187a6784",
+    "url": "/Nimble_test-task/static/js/main.876ad9d9.chunk.js"
+  },
+  {
+    "revision": "d5f7820586c1ece59a75",
+    "url": "/Nimble_test-task/static/js/runtime~main.2993ae40.js"
+  }
+]);
